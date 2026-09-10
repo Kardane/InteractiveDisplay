8b1a0c7cb8e1a479f84d7d8f6c58fd3b74c9d1a6
Kardane <64183602+Kardane@users.noreply.github.com>
2026-09-10 22:16:28 +0900

    feat(api): expose group registrar access

--- src/main/java/com/interactivedisplay/api/InteractiveDisplayRegistrar.java
+++ src/main/java/com/interactivedisplay/api/InteractiveDisplayRegistrar.java
@@ -3,10 +3,13 @@ package com.interactivedisplay.api;
 import com.interactivedisplay.api.action.ActionApi;
 import com.interactivedisplay.api.callback.CallbackApi;
 import com.interactivedisplay.api.event.EventApi;
+import com.interactivedisplay.api.group.GroupApi;
 import com.interactivedisplay.api.window.WindowApi;
 
 public interface InteractiveDisplayRegistrar {
     WindowApi windows();
+
+    GroupApi groups();
 
     CallbackApi callbacks();
 
