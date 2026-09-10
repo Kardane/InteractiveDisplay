0653ab40a69b4d971ca4eb303803b33499c53a9b
Kardane <64183602+Kardane@users.noreply.github.com>
2026-09-10 17:47:37 +0900

    feat(api): expose action and event registries

--- src/main/java/com/interactivedisplay/api/InteractiveDisplayRegistrar.java
+++ src/main/java/com/interactivedisplay/api/InteractiveDisplayRegistrar.java
@@ -1,10 +1,16 @@
 package com.interactivedisplay.api;
 
+import com.interactivedisplay.api.action.ActionApi;
 import com.interactivedisplay.api.callback.CallbackApi;
+import com.interactivedisplay.api.event.EventApi;
 import com.interactivedisplay.api.window.WindowApi;
 
 public interface InteractiveDisplayRegistrar {
     WindowApi windows();
 
     CallbackApi callbacks();
+
+    ActionApi actions();
+
+    EventApi events();
 }
